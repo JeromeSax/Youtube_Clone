3e973be1174424487382968272aa505b14211781
unknown <jeromesloss@yahoo.com>
2023-07-24 09:09:19 -0400

    Video details being added

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
+import { useParam } from 'react-router-dom';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Videos } from './';
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParam();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=`)
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
      .then((data) => setVideos(data.items))
-  }, []);
+  }, [searchTerm]);
 
   return (
     <Box p={2} sx={{ overflowY: 'auto',
@@ -18,15 +20,15 @@ const SearchFeed = () => {
       fontWeight="bold" md={2} sx={{
       color: 'white' }}>
        Search Results for: <span style={{ color: '#F31503'}}
-        >videos</span>
+        >{searchTerm}</span> Videos
       </Typography>
 
       <Videos videos={videos}/>
     </Box>
 
 
-    </Stack>
+    
   )
 }
 
-export default Feed
+export default SearchFeed;
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Stack, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { Video } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+      <Stack></Stack>
+    </Box>
   )
 }
 
-export default VideoDetail
+
+
+export default VideoDetail;
